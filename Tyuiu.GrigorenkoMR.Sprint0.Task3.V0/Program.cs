6acdc4115efc2f9c88f3afd9412d545bb56d628a
Matvey Grigorenko <Tyuiu.GrigorenkoMR@outlook.com>
2026-09-13 16:51:40 +0500

    Task3

---
+++ Tyuiu.GrigorenkoMR.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.GrigorenkoMR.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.GrigorenkoMR.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
