82ec2735521a71000e646b5f2a72912bdd7b9add
Matvey Grigorenko <Tyuiu.GrigorenkoMR@outlook.com>
2026-09-14 21:26:37 +0500

    Task6

---
+++ Tyuiu.GrigorenkoMR.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.GrigorenkoMR.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.GrigorenkoMR.Sprint0.Task6.V0.Test
+{
+    public class DataServiceTest
+    {
+        [Fact]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.Equal(15, res);
+        }
+        [Fact]
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.Equal(-15, res);
+        }
+        [Fact]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.Equal(120, res);
+        }
+    }
+}
---
+++ Tyuiu.GrigorenkoMR.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GrigorenkoMR.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.GrigorenkoMR.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма элементов массива * " + DataService.AdditionArray(new int[] { 1, 2, 3, 4, 5 }));
+            Console.WriteLine("Разность элементов массива * " + DataService.SubtractionArray(new int[] { 1, 2, 3, 4, 5 }));
+            Console.WriteLine("Произведение элементов массива * " + DataService.MultiplicationArray(new int[] { 1, 2, 3, 4, 5 }));
+            Console.ReadKey();
+        }
+    }
+}
